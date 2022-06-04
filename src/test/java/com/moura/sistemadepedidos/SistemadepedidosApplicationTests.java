992484457e3b867dcbd5c157fc04d4c545d5c1cf
Mateus Moura <mateus51moura@gmail.com>
2022-06-03 21:16:00 -0300

    Project created

---
+++ src/test/java/com/moura/sistemadepedidos/SistemadepedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura.sistemadepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadepedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
