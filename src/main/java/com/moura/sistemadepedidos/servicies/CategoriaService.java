90c4d37cdbbcb03ff6c0d05cb51af761681145cb
Mateus Moura <mateus51moura@gmail.com>
2022-07-20 23:11:13 -0300

    Syntactic validation

--- src/main/java/com/moura/sistemadepedidos/servicies/CategoriaService.java
+++ src/main/java/com/moura/sistemadepedidos/servicies/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.moura.sistemadepedidos.domain.Categoria;
+import com.moura.sistemadepedidos.dto.CategoriaDTO;
 import com.moura.sistemadepedidos.repositories.CategoriaRepository;
 import com.moura.sistemadepedidos.servicies.exceptions.DataIntegrityException;
 import com.moura.sistemadepedidos.servicies.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
