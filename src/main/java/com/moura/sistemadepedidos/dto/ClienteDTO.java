5f0c5083b577b1a6bacad1c618bb1a51f772de91
Mateus Moura <mateus51moura@gmail.com>
2022-08-20 17:39:35 -0300

    Custom validation: email not repeated on Client update

--- src/main/java/com/moura/sistemadepedidos/dto/ClienteDTO.java
+++ src/main/java/com/moura/sistemadepedidos/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.moura.sistemadepedidos.domain.Cliente;
+import com.moura.sistemadepedidos.servicies.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
