f6541db2646aceb78f7f3f496977ed397e5d2678
Mateus Moura <mateus51moura@gmail.com>
2022-10-07 19:17:08 -0300

    Inserting Order

--- src/main/java/com/moura/sistemadepedidos/domain/PagamentoComCartao.java
+++ src/main/java/com/moura/sistemadepedidos/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.moura.sistemadepedidos.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.moura.sistemadepedidos.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/moura/sistemadepedidos/servicies/PedidoService.java
+++ src/main/java/com/moura/sistemadepedidos/servicies/PedidoService.java
@@ -1,11 +1,17 @@
 package com.moura.sistemadepedidos.servicies;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.moura.sistemadepedidos.domain.ItemPedido;
+import com.moura.sistemadepedidos.domain.PagamentoComBoleto;
 import com.moura.sistemadepedidos.domain.Pedido;
+import com.moura.sistemadepedidos.domain.enums.EstadoPagamento;
+import com.moura.sistemadepedidos.repositories.ItemPedidoRepository;
+import com.moura.sistemadepedidos.repositories.PagamentoRepository;
 import com.moura.sistemadepedidos.repositories.PedidoRepository;
 import com.moura.sistemadepedidos.servicies.exceptions.ObjectNotFoundException;
 
@@ -15,9 +21,41 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(), null));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamento(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
