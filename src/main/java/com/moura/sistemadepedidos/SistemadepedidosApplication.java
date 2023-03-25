45b31a94282cde810b3a5fa3e2fb27c88a77c4d2
Mateus Moura <mateus51moura@gmail.com>
2023-03-25 13:25:36 -0300

    Saving first file to S3

--- src/main/java/com/moura/sistemadepedidos/SistemadepedidosApplication.java
+++ src/main/java/com/moura/sistemadepedidos/SistemadepedidosApplication.java
@@ -1,17 +1,24 @@
 package com.moura.sistemadepedidos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.moura.sistemadepedidos.servicies.S3Service;
+
 @SpringBootApplication
 public class SistemadepedidosApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemadepedidosApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {		
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Projetos\\Spring_boot\\fotos_s3\\ana.jfif");
 	}
 }
