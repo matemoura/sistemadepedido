185292d72265388db2ed7438a3b06c95efc10e7f
Mateus Moura <mateus51moura@gmail.com>
2023-03-25 21:30:21 -0300

    Handling exceptions properly

---
+++ src/main/java/com/moura/sistemadepedidos/servicies/exceptions/FileException.java
@@ -0,0 +1,14 @@
+package com.moura.sistemadepedidos.servicies.exceptions;
+
+public class FileException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
